e531bdef96401ee59458da4e7ed3f8b3d5997f28
Steven Austin McVicker <amcvic@airtop.lan>
2025-09-04 14:27:19 -0400

    add customer ordering

--- main/customer/customer.go
+++ main/customer/customer.go
@@ -17,3 +17,42 @@ embedded_components {
   "}\n"
   ""
 }
+embedded_components {
+  id: "speech_bubble"
+  type: "sprite"
+  data: "default_animation: \"speechbubble\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/customer/food.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    y: 109.0
+  }
+  scale {
+    x: 0.5
+    y: 0.5
+  }
+}
+embedded_components {
+  id: "thinking_dots"
+  type: "sprite"
+  data: "default_animation: \"thinking\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/customer/food.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    y: 175.0
+    z: 1.0
+  }
+}
+embedded_components {
+  id: "food_factory"
+  type: "factory"
+  data: "prototype: \"/main/customer/food.go\"\n"
+  ""
+}
